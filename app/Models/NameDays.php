3c7d39938cf31763880c23843ee87dd7d545749d
Matej Hrnciarik <hrnciarik@aroha.sk>
2021-12-20 23:23:29 +0100

    add model NameDays and db migrations

---
+++ app/Models/NameDays.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NameDays extends Model
+{
+    use HasFactory;
+}
+
+// todo: https://raw.githubusercontent.com/zoltancsontos/slovak-name-days-json/master/slovak-nameday-list-shorter.json
---
+++ database/migrations/2021_12_20_214043_create_name_days_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNameDaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('name_days', function (Blueprint $table) {
+            $table->id();
+            $table->date('day')->index()->nullable(false)->unique();
+            $table->string('name', '250')->index()->nullable(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('name_days');
+    }
+
+}
